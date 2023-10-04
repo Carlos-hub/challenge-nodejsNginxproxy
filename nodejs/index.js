1d42a7bd167c74633f0899b5f2447f13317470f5
carlos-hub <delaracarlosmiguel@gmail.com>
2023-10-04 20:48:31 -0300

    ajustando erros nodejs

--- nodejs/index.js
+++ nodejs/index.js
@@ -14,49 +14,85 @@ const mysql = require('mysql');
 const connection = mysql.createConnection(config);
 
 
-const verifyExistTable = `SELECT 1 FROM people LIMIT 1`
-const tableExist =  connection.query(verifyExistTable,async (err, results)=>{
-    console.log(await results);
-    if(results.length >0){
-        console.log('table ok');
-    }else{
-        const createTable = `CREATE TABLE people (
-            id INT NOT NULL AUTO_INCREMENT,
-            name VARCHAR(255) NOT NULL,
-            PRIMARY KEY (id)
-        );`
-        connection.query(createTable);
-}});
+const verifyExistTable = `SHOW TABLES LIKE 'people';`
+const tableExist =  connection.query(verifyExistTable);
+if(tableExist){
+    console.log('table ok');
+}else{
+    const createTable = `CREATE TABLE people (
+        id INT NOT NULL AUTO_INCREMENT,
+        name VARCHAR(255) NOT NULL,
+        PRIMARY KEY (id)
+    );`
+    connection.query(createTable);
+}
     
 
-app.get('/',async (req,res)=>{
-
-const sqli = `INSERT INTO people(name) values('Carlos')`
-connection.query(sqli);
-
-    const sql = `SELECT * FROM people`
-    connection.query(sql, (err, results) => {
-        if (err) {
-          console.error('Erro na consulta: ' + err.message);
-          connection.end(); // Feche a conexão em caso de erro
-          return;
-        }
-    
-        // Iterar pelos resultados e imprimir campos individuais
-        const retornaClient = [];
-        results.forEach((row) => {
-          retornaClient.push(row.id, row.name);
+function tableExists(tableName) {
+    return new Promise((resolve, reject) => {
+        const verifyExistTable = `SHOW TABLES LIKE ?`;
+        connection.query(verifyExistTable, [tableName], (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results.length > 0);
+            }
         });
-        return res.json(`
-        <h1> FULL CYCLE ROCKS!!!<br/>
-        ${retornaClient}
-        `)
-      });
-    //   return res.send("<h1> FULL CYCLE ROCKS!!!")
+    });
+}
+
+// Função para criar a tabela
+function createTable() {
+    const createTableQuery = `CREATE TABLE people (
+        id INT NOT NULL AUTO_INCREMENT,
+        name VARCHAR(255) NOT NULL,
+        PRIMARY KEY (id)
+    );`;
+    return new Promise((resolve, reject) => {
+        connection.query(createTableQuery, (error) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve();
+            }
+        });
+    });
+}
+
+app.get('/', async (req, res) => {
+    try {
+        const exists = await tableExists('people');
+        if (!exists) {
+            await createTable();
+        }
+
+        const sqli = `INSERT INTO people(name) VALUES('Carlos')`;
+        connection.query(sqli);
+
+        const sql = `SELECT * FROM people`;
+        connection.query(sql, (err, results) => {
+            if (err) {
+                console.error('Erro na consulta: ' + err.message);
+                connection.end();
+                return res.status(500).json({ error: 'Erro na consulta.' });
+            }
+
+            const retornaClient = [];
+            results.forEach((row) => {
+                retornaClient.push(row.id, row.name);
+            });
+
+            return res.send(`
+                <h1> FULL CYCLE ROCKS!!!<br/>
+                ${retornaClient.join(', ')}
+            `);
+        });
+    } catch (error) {
+        console.error('Erro: ' + error.message);
+        return res.status(500).json({ error: 'Erro interno.' });
+    }
 });
 
-
-
-app.listen(3000,()=>{
-    console.log('olá mundo');
+app.listen(3000, () => {
+    console.log('Servidor ouvindo na porta 3000');
 });
